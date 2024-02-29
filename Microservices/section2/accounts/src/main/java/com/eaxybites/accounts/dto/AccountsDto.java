32138b5ac4038240425feaf0a5c0617b3855e028
Atul Raj <rajatul53@gmail.com>
2024-02-29 23:07:02 +0530

    Implemented input data validations.

--- Microservices/section2/accounts/src/main/java/com/eaxybites/accounts/dto/AccountsDto.java
+++ Microservices/section2/accounts/src/main/java/com/eaxybites/accounts/dto/AccountsDto.java
@@ -1,13 +1,19 @@
 package com.eaxybites.accounts.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotNull(message = "Account Number cannot be empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account Number must be 10 digits.")
     private Long accountNumber;
 
+    @NotNull(message = "Account Type cannot be empty")
     private String accountType;
 
+    @NotNull(message = "Branch Address cannot be empty")
     private String branchAddress;
 }
--- Microservices/section2/accounts/src/main/java/com/eaxybites/accounts/dto/CustomerDto.java
+++ Microservices/section2/accounts/src/main/java/com/eaxybites/accounts/dto/CustomerDto.java
@@ -1,14 +1,25 @@
 package com.eaxybites.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 
 @Data
 public class CustomerDto {
 
+    @NotNull(message = "Name cannot be empty")
+    @Size(min = 5,max = 30, message = "Name should be within 5 and 30 characters")
     private String name;
 
+    @Email(message = "Email address should be valid")
+    @NotNull(message = "email cannot be empty")
     private String email;
 
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile Number must be 10 digits.")
+    @NotNull(message = "Mobile Number cannot be empty")
     private String mobileNumber;
 
     private AccountsDto accountsDto;
